e12336ca0490147cbda291a84c6772d55264bd84
Nishant Bansal <bansal.nishant18052000@gmail.com>
2019-10-26 14:37:32 +0530

    Pay button complete and balance button partiall done

--- src/userDetails.java
+++ src/userDetails.java
@@ -54,34 +54,57 @@ public class userDetails {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try(Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/fees","root","contactmrnishantbansal@18");){
-                    float amount=Float.parseFloat(JOptionPane.showInputDialog(frame, "Payment"));
-                    String query="select paid from records where name=\""+name+"\" and password=\""+pass+"\"";
+                    double amount=Double.parseDouble(JOptionPane.showInputDialog(frame, "Payment"));
+                    String query="select * from records where name=\""+name+"\" and password=\""+pass+"\"";
                     Statement stmt1=con.createStatement();
                     ResultSet rst=stmt1.executeQuery(query);
+                    double balance=-1d;
                     while(rst.next()){
-                        if(rst.getFloat("paid")==100){
+                        balance=rst.getDouble("balance");
+                        if(balance==0d){
                             JOptionPane.showMessageDialog(frame, "No need to pay.\nComplete fees payment is already done.");
-                            break;
+                            break;                   
                         }
                         else{
-                            float balance=rst.getFloat("paid");
-                            if(amount<=100){
-                                float total_amount_update=rst.getFloat("paid")+amount;
-                                query="update records set paid="+total_amount_update+" where name=\""+name+"\" and password=\""+pass+"\"";
+                            if(amount<=rst.getDouble("balance")){
+                                double updated_amount=rst.getDouble("paid")+amount;
+                                query="update records set paid="+updated_amount+" where name=\""+name+"\" and password=\""+pass+"\"";
                                 Statement stmt=con.createStatement();
                                 stmt.execute(query);
                                 JOptionPane.showMessageDialog(frame, "Paid Successfully...");
+                                balance-=amount;
+                                String query2="update records set balance="+balance+" where name=\""+name+"\" and password=\""+pass+"\"";
+                                Statement stmt2=con.createStatement();
+                                stmt2.execute(query2);
                                 break;
                             }
                             else{
                                 JOptionPane.showMessageDialog(frame, "Balance amount is less.("+balance+")");
                                 break;
-                            }
+                            }    
                         }                                             
                     }
+                    
                 con.close();
                 }catch(Exception ee){
                     System.out.println("Exception caught in pay button listener in userDetais.java"+ee);
+                }
+            }
+        });
+        
+        balance.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try(Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/fees","root","contactmrnishantbansal@18");){
+                    String query="select balance from records where name=\""+name+"\" and password=\""+pass+"\"";
+                    Statement stmt=con.createStatement();
+                    ResultSet rst=stmt.executeQuery(query);
+                    while(rst.next()){
+                        JOptionPane.showMessageDialog(frame, "BALANCE: "+rst.getFloat("balance"));
+                        break;
+                    }
+                }catch(Exception ee){
+                    System.out.println("Exception caught in balnace button listener in userDetails.java");
                 }
             }
         });
